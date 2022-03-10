4042019cf3f97f8d1d22568729af80284986e1d6
AyanamiAsuka <fyb0118@gmail.com>
2022-03-10 20:21:19 +0900

    尚硅谷React学习 DAY - 15

---
+++ 14_react脚手架项目/06-antd-demo/src/App.js
@@ -0,0 +1,17 @@
+
+import { Button } from 'antd'
+import 'antd/dist/antd.css'
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        App...
+        <Button type='primary'>点我</Button>
+        <Button>点我</Button>
+      </div>
+    )
+  }
+}
